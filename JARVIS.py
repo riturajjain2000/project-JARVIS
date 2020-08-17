8d67c39b9c71a219bd6884771923125c5fdc4c92
Rituraj Jain <rrjain20122000@gmail.com>
2020-08-17 19:32:54 +0530

    Update JARVIS.py

--- JARVIS.py
+++ JARVIS.py
@@ -28,7 +28,7 @@ def wishMe():
     else:
         speak("Good Evening!")  
 
-    speak("I am Jarvis ,  Roodraksh     Sir. Please tell me how may I help you")       
+    speak("I am Jarvis ,  Rituraj     Sir. Please tell me how may I help you")       
 
 def takeCommand():
     #It takes microphone input from the user and returns string output
