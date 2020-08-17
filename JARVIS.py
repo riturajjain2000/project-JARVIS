ed6625986640c12a94e9a25da79cdbee93f3c079
Rituraj Jain <rrjain20122000@gmail.com>
2020-08-17 19:37:03 +0530

    Update JARVIS.py

--- JARVIS.py
+++ JARVIS.py
@@ -106,4 +106,4 @@ if __name__ == "__main__":
                 speak("Email has been sent!")
             except Exception as e:
                 print(e)
-                speak("Sorry my friend harry bhai. I am not able to send this email")    
+                speak("Sorry Rituraj sir. I am not able to send this email")    
